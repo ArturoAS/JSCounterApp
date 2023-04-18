c4dceb259b8f5007b07d39a0f3bb23d7ada837c8
ArturoAS1 <arturo.andres.silva@gmail.com>
2023-04-18 16:04:03 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let count = 0;
+const countAddBtn = document.getElementById("add")
+const countSubBtn = document.getElementById("sub")
+
+countAddBtn.addEventListener("click", () => {
+    console.log("hello from the add button")
+})
