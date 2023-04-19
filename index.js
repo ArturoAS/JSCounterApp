f2ec7927c1a1ece76b724b036e0bf13c1cd5ebc2
ArturoAS1 <arturo.andres.silva@gmail.com>
2023-04-19 15:18:18 +0200

    added full funcionality

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let count = 0;
+const countHeader = document.getElementById("count")
 const countAddBtn = document.getElementById("add")
 const countSubBtn = document.getElementById("sub")
 
+countHeader.innerText = count
+
 countAddBtn.addEventListener("click", () => {
-    console.log("hello from the add button")
+    count++;
+    countHeader.innerText = count
+})
+countSubBtn.addEventListener("click", () => {
+    count--;
+    countHeader.innerText = count
 })
